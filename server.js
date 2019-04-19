7cf6cde4c514194600a8e7f28d40e785bf669ffe
adiulay <95jvt71@gmail.com>
2019-04-19 10:39:40 -0700

    startup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const axios = require('axios');
+const _ = require('lodash');
+const port = process.env.PORT || 8080;
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+    response.send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on the port ${port}`);
+});
